4cedef8c34aec46b1691d83526029aae68506a7c
Valentin Vasilev <valentin.v1997@gmail.com>
2017-05-20 14:20:22 +0300

    Events will be disconnected after leaving activity

--- PocketStatistician/ExcerptFieldsActivity.cs
+++ PocketStatistician/ExcerptFieldsActivity.cs
@@ -19,6 +19,8 @@ namespace PocketStatistician
         public static double[][] intervals;
         public static double[] Xi, Yi;
         private bool continuable = true;
+        private EditText[] Field;
+        private Button ProcDataBT;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -43,95 +45,102 @@ namespace PocketStatistician
                 else
                     field[i].Hint = $"{i + 1}. Xi Yi";
             }
-
+            Field = field;
             var procDataBT = FindViewById<Button>(Resource.Id.procDataBT);
+            ProcDataBT = procDataBT;
             #endregion
 
             #region Events
-            procDataBT.Click += delegate
-             {
-                 #region Data assigning and format check
-                 string[] split = null;
-                 if (MainActivity.hasIntervals && MainActivity.SpinnerPos == (int)MainActivity.AnalysisType.OneDA)                       // This is used only for the One-Dimention Analysis. Other analysis work with two parameters.
-                 {
-                     intervals = new double[2][];
-                     intervals[0] = new double[field.Length];
-                     intervals[1] = new double[field.Length];
-
-                     split = new string[3];
-                     Yi = new double[field.Length];
-                 }
-                 else
-                 {
-                     split = new string[2];
-                     Xi = new double[field.Length];
-                     Yi = new double[field.Length];
-                 }
-
-                 for (int i = 0; i < field.Length; i++)
-                 {
-                     if (MainActivity.hasIntervals && MainActivity.SpinnerPos == (int)MainActivity.AnalysisType.OneDA)
-                     {
-                         continuable = field[i].Text.Contains("~") && field[i].Text.Split().Length == 2;         // checking if the format is correct
-                         if (!continuable)
-                             break;
-
-                         string[] twoStrings = field[i].Text.Split();
-                         split[0] = twoStrings[0].Split('~')[0];
-                         split[1] = twoStrings[0].Split('~')[1];
-                         split[2] = twoStrings[1];
-
-                         continuable = double.TryParse(split[0], out intervals[0][i]) &&
-                         double.TryParse(split[1], out intervals[1][i]) &&
-                         double.TryParse(split[2], out Yi[i]);
-                         if (!continuable)
-                             break;
-                     }
-                     else
-                     {
-                         continuable = field[i].Text.Split().Length == 2;         // checking if the format is correct
-                         if (!continuable)
-                             break;
-
-                         split[0] = field[i].Text.Split()[0];
-                         split[1] = field[i].Text.Split()[1];
-
-                         continuable = double.TryParse(split[0], out Xi[i]) && double.TryParse(split[1], out Yi[i]);
-                         if (!continuable)
-                             break;
-                     }
-
-                 }
-
-                #endregion
-
-                 if(!continuable)
-                 {
-                     ShowDialog("Your data is wrongly formated.\nPlease check and try again.");
-                     return;
-                 }
-                 else
-                 {
-                     switch(MainActivity.SpinnerPos)
-                     {
-                         case (int)MainActivity.AnalysisType.OneDA:
-                             ResultActivity.ODA = MainActivity.hasIntervals ?
-                             new Analizers.One_dim_analysis(field.Length, intervals[0], intervals[1], Yi) :
-                             new Analizers.One_dim_analysis(field.Length, Xi, Yi);
-                             break;
-
-                         case (int)MainActivity.AnalysisType.RegrCorA:
-                             ResultActivity.RA = new Analizers.Regression_analysis(field.Length, Xi, Yi);
-                             break;
-                     }
-
-                     var intent = new Intent(this, typeof(ResultActivity));
-                     StartActivity(intent);
-                     ((Activity)MainActivity.MainActivityContext).Finish();
-                     Finish();
-                 } 
-             };
+            procDataBT.Click += procDataBT_Click;
+             
             #endregion
+        }
+
+
+        private void procDataBT_Click(object sender, EventArgs e)
+        {
+            #region Data assigning and format check
+            string[] split = null;
+            if (MainActivity.hasIntervals && MainActivity.SpinnerPos == (int)MainActivity.AnalysisType.OneDA)                       // This is used only for the One-Dimention Analysis. Other analysis work with two parameters.
+            {
+                intervals = new double[2][];
+                intervals[0] = new double[Field.Length];
+                intervals[1] = new double[Field.Length];
+
+                split = new string[3];
+                Yi = new double[Field.Length];
+            }
+            else
+            {
+                split = new string[2];
+                Xi = new double[Field.Length];
+                Yi = new double[Field.Length];
+            }
+
+            for (int i = 0; i < Field.Length; i++)
+            {
+                if (MainActivity.hasIntervals && MainActivity.SpinnerPos == (int)MainActivity.AnalysisType.OneDA)
+                {
+                    continuable = Field[i].Text.Contains("~") && Field[i].Text.Split().Length == 2;         // checking if the format is correct
+                    if (!continuable)
+                        break;
+
+                    string[] twoStrings = Field[i].Text.Split();
+                    split[0] = twoStrings[0].Split('~')[0];
+                    split[1] = twoStrings[0].Split('~')[1];
+                    split[2] = twoStrings[1];
+
+                    continuable = double.TryParse(split[0], out intervals[0][i]) &&
+                    double.TryParse(split[1], out intervals[1][i]) &&
+                    double.TryParse(split[2], out Yi[i]);
+                    if (!continuable)
+                        break;
+                }
+                else
+                {
+                    continuable = Field[i].Text.Split().Length == 2;         // checking if the format is correct
+                    if (!continuable)
+                        break;
+
+                    split[0] = Field[i].Text.Split()[0];
+                    split[1] = Field[i].Text.Split()[1];
+
+                    continuable = double.TryParse(split[0], out Xi[i]) && double.TryParse(split[1], out Yi[i]);
+                    if (!continuable)
+                        break;
+                }
+
+            }
+
+            #endregion
+
+            if (!continuable)
+            {
+                ShowDialog("Your data is wrongly formated.\nPlease check and try again.");
+                return;
+            }
+            else
+            {
+                switch (MainActivity.SpinnerPos)
+                {
+                    case (int)MainActivity.AnalysisType.OneDA:
+                        ResultActivity.ODA = MainActivity.hasIntervals ?
+                        new Analizers.One_dim_analysis(Field.Length, intervals[0], intervals[1], Yi) :
+                        new Analizers.One_dim_analysis(Field.Length, Xi, Yi);
+                        break;
+
+                    case (int)MainActivity.AnalysisType.RegrCorA:
+                        ResultActivity.RA = new Analizers.Regression_analysis(Field.Length, Xi, Yi);
+                        break;
+                }
+
+                var intent = new Intent(this, typeof(ResultActivity));
+                StartActivity(intent);
+                ((Activity)MainActivity.MainActivityContext).Finish();
+                Finish();
+                MainActivity.DisconnectEvents();
+                ProcDataBT.Click -= procDataBT_Click;
+            }
         }
 
         private void ShowDialog(string text)
--- PocketStatistician/MainActivity.cs
+++ PocketStatistician/MainActivity.cs
@@ -24,6 +24,16 @@ namespace PocketStatistician
         public static int SpinnerPos = 0;
         public static Context MainActivityContext;
         #endregion
+
+        #region Private Controls
+        private static LinearLayout OptionalQuestionLayout;
+        private static Button NextBT;
+        private static EditText FieldsSize;
+        private static RadioButton[] RB;
+        private static Spinner AnalysisSwitch;
+        private static EventHandler<AdapterView.ItemSelectedEventArgs> SpinnerEventHandler;
+        #endregion
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -44,30 +54,21 @@ namespace PocketStatistician
 
             #region Declarations and predefinements
             optionalQuestionLayout.Visibility = SpinnerPos==0? ViewStates.Visible:ViewStates.Invisible;
+            OptionalQuestionLayout = optionalQuestionLayout;
             nextBT.Enabled = false;
+            NextBT = nextBT;
+            FieldsSize = fieldsSize;
+            RB = rb;
+            AnalysisSwitch = analysisSwitch;
             #endregion
 
             #region Events
             ActivateSpinner(analysisSwitch, Resource.Array.anayisis_array);
-            analysisSwitch.ItemSelected += delegate
-              {
-                  optionalQuestionLayout.Visibility = SpinnerPos == (int)AnalysisType.OneDA ? ViewStates.Visible : ViewStates.Invisible;
-              };
 
-            fieldsSize.TextChanged += delegate
-              {
-                  nextBT.Enabled = fieldsSize.Text != "" &&
-                  int.TryParse(fieldsSize.Text, out ExcerptFieldsActivity.NumberOfFields) &&
-                  !fieldsSize.Text.Contains("-") &&
-                  ExcerptFieldsActivity.NumberOfFields > 2 ? true : false;
-              };
+            analysisSwitch.ItemSelected += analysisSwitch_ItemSelected;
+            fieldsSize.TextChanged += fieldsSize_TextChange;
+            nextBT.Click += nextBT_click;
 
-            nextBT.Click += delegate
-              {
-                  hasIntervals = rb[0].Checked;
-                  var intent = new Intent(this, typeof(ExcerptFieldsActivity));
-                  StartActivity(intent);
-              };
             #endregion
         }
 
@@ -76,18 +77,47 @@ namespace PocketStatistician
         #region Spinner
         private void ActivateSpinner(Spinner spinner, int textArrayResld)
         {
-            spinner.ItemSelected += new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            SpinnerEventHandler = new EventHandler<AdapterView.ItemSelectedEventArgs>(spinner_ItemSelected);
+            spinner.ItemSelected += SpinnerEventHandler;
             var adapter = ArrayAdapter.CreateFromResource(this, textArrayResld, Android.Resource.Layout.SimpleSpinnerItem);
             adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
             spinner.Adapter = adapter;
         }
+        #endregion
 
-        private void spinner_ItemSelected (object sender, AdapterView.ItemSelectedEventArgs e)
+        #region Events
+        private static void spinner_ItemSelected (object sender, AdapterView.ItemSelectedEventArgs e)
         {
             Spinner spinner = (Spinner)sender;
             SpinnerPos = e.Position;
         }
 
+        private static void analysisSwitch_ItemSelected(object sender, EventArgs e) => 
+            OptionalQuestionLayout.Visibility = SpinnerPos == (int)AnalysisType.OneDA ? ViewStates.Visible : ViewStates.Invisible;
+
+        private static void fieldsSize_TextChange(object sender, EventArgs e)
+        {
+            NextBT.Enabled = FieldsSize.Text != "" &&
+            int.TryParse(FieldsSize.Text, out ExcerptFieldsActivity.NumberOfFields) &&
+            !FieldsSize.Text.Contains("-") &&
+            ExcerptFieldsActivity.NumberOfFields > 2 ? true : false;
+        }
+
+        private static void nextBT_click(object sender, EventArgs e)
+        {
+            hasIntervals = RB[0].Checked;
+            var intent = new Intent(NextBT.Context, typeof(ExcerptFieldsActivity));
+            NextBT.Context.StartActivity(intent);
+        }
+
+        public static void DisconnectEvents()
+        {
+            AnalysisSwitch.ItemSelected -= analysisSwitch_ItemSelected;
+            AnalysisSwitch.ItemSelected -= SpinnerEventHandler;
+            FieldsSize.TextChanged -= fieldsSize_TextChange;
+            NextBT.Click -= nextBT_click;
+
+        }
         #endregion
 
         #endregion
